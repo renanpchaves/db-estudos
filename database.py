f5b459cfd955d38b0488d6e27b52df55fb705059
renanpchaves <nanerpc@gmail.com>
2026-04-11 10:38:15 -0300

    feat: .env.example adicionado + validacao database_url

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE = os.getenv("DATABASE_URL")
+if not DATABASE:
+    raise ValueError("DATABASE_URL não encontrada no .env")
 
 engine = create_engine(DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
